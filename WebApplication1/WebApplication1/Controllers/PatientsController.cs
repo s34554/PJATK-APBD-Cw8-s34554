e58af1fb240361aa0fe50574400e7bd47a1922c1
Szymon Bak <s34554@pjwskt.edu.pl>
2026-05-18 22:03:40 +0200

    Add controler

--- WebApplication1/WebApplication1/Controllers/PatientsController.cs
+++ WebApplication1/WebApplication1/Controllers/PatientsController.cs
@@ -1,6 +1,32 @@
-﻿namespace WebApplication1.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DTOs;
+using WebApplication1.Exceptions;
+using WebApplication1.Services;
 
-public class PatientsController
+namespace WebApplication1.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class PatientsController(IPatientService service): ControllerBase
 {
-    
+    [HttpGet]
+    public async Task<IActionResult> GetPatients([FromQuery] string? search)
+    {
+        var patients = await service.GetPatientsAsync(search);
+        return Ok(patients);
+    }
+    [HttpPost("{pesel}/bedassignments")]
+    public async Task<IActionResult> AssignBed(string pesel, [FromBody] CreateBedAssignmentDto dto)
+    {
+        try
+        {
+            var id = await service.AssignBedAsync(pesel, dto);
+            return Created($"/api/patients/{pesel}/bedassignments/{id}", new { id });
+        }
+        catch (NotFoundException ex)
+        {
+            return NotFound(ex.Message);
+        }
+    }
 }
