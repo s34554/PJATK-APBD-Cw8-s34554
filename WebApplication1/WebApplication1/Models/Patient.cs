f29397dc9a0422c8f0c602cec3ce5d9c7f716a8f
Szymon Bak <s34554@pjwskt.edu.pl>
2026-05-18 20:06:52 +0200

    Scaffold Database

---
+++ WebApplication1/WebApplication1/Models/Patient.cs
@@ -0,0 +1,18 @@
+﻿namespace WebApplication1.Models;
+
+public partial class Patient
+{
+    public string Pesel { get; set; } = null!;
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public int Age { get; set; }
+
+    public bool Sex { get; set; }
+
+    public virtual ICollection<Admission> Admissions { get; set; } = new List<Admission>();
+
+    public virtual ICollection<BedAssignment> BedAssignments { get; set; } = new List<BedAssignment>();
+}
