dc83796fd83622a8cd0ca1f579c1758c9f6a156a
Szymon Bak <s34554@pjwskt.edu.pl>
2026-05-18 22:38:11 +0200

    Fix App file names leaking when empty fields in POST json

--- WebApplication1/WebApplication1/Program.cs
+++ WebApplication1/WebApplication1/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using WebApplication1.Data;
@@ -16,6 +17,38 @@ builder.Services.AddDbContext<HospitalContext>(opt =>
 builder.Services.AddScoped<IPatientService, PatientService>();
 builder.Services.AddExceptionHandler<NotFoundExceptionHandler>();
 builder.Services.AddProblemDetails();
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var invalidFields = context.ModelState
+            .Where(kvp => kvp.Value?.Errors.Count > 0)
+            .Select(kvp =>
+            {
+                var key = kvp.Key.StartsWith("$.") ? kvp.Key[2..] : kvp.Key;
+                if (string.IsNullOrEmpty(key)) key = "body";
+
+                var hasParseError = kvp.Value!.Errors
+                    .Any(e => e.ErrorMessage.Contains("could not be converted"));
+
+                return new
+                {
+                    field = key,
+                    error = hasParseError
+                        ? "Invalid value for this field."
+                        : "This field is required or invalid."
+                };
+            })
+            .ToArray();
+
+        return new BadRequestObjectResult(new
+        {
+            message = "The request body is invalid.",
+            errors = invalidFields
+        });
+    };
+});
+
 
 var app = builder.Build();
 
